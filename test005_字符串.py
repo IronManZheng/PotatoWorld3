78a604291a41a8fe0bc7f411eb88f3cea07bd527
IronManZheng <lovebreakintao@163.com>
2018-03-16 16:59:47 +0800

    This is Message.

---
+++ test005_字符串.py
@@ -0,0 +1,14 @@
+var1 = 'Hello World'
+var2 = "Hello my World"
+
+if('e' in var1):
+    print('Hello World have e')
+if('a' not in var1):
+    print('Hello World dont have a')
+
+print("var1[1:8]:"+var1[1:8])
+print('\a')
+print('var2[1:8]:'*3+var2[1:8]*3)
+
+
+#字符串的定义可以用单引号或者双引号
